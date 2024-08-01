6ed838da36e456c0a27ce2390d519195f6765789
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-01 14:24:42 -0300

    Ao clicar em um dos quadrado a letra X é renderizada dentro do quadrado

--- tool-manager/src/App.js
+++ tool-manager/src/App.js
@@ -1,22 +1,41 @@
-export default function Square() {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button 
+     className="square" 
+     onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <>
-      <div>
-        <button className="square">X</button>
-        <button classsName="square">X</button>
-        <button className="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
 
-      <div>
-        <button classsName="square">X</button>
-        <button className="square">X</button>
-        <button classsName="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
 
-      <div>
-        <button className="square">X</button>
-        <button classsName="square">X</button>
-        <button classsName="square">X</button>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
